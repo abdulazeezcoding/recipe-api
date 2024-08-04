b6a89c102b45c318bc55799cb9d46be66667078b
Abdul-Aziz Mohammed <159774132+abdulazeezcoding@users.noreply.github.com>
2024-08-04 11:10:46 +0000

    add descriptive comments to routes

--- routes/recipes.routes.js
+++ routes/recipes.routes.js
@@ -8,13 +8,17 @@ const upload = multer({dest: 'uploads/images'});
 // Create recipes router
 export const router = Router();
 
-// Post data
+// Create a recipe
 router.post("/", upload.single('image'), addRecipe);
 
+// Fetch all recipes
 router.get("/", getRecipes);
 
+// Fetch a recipe using it's ID
 router.get("/:id", getRecipe);
 
+// Update recipe data
 router.patch("/:id", updateRecipe);
 
+// Delete a recipe
 router.delete("/:id", deleteRecipe);
